0bad5e48cdb3662293e574a9bd3c81c5b7d1677a
Perry Vorselaars <perry@perigee.xyz>
2021-03-22 18:40:57 +0100

    Removed obsolete function

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -4,15 +4,8 @@ function prompt {$((Get-Location).Path -split "\\" | Select-Object -last 1) + " 
 # Set terminal editing mode to vi
 Set-PSReadLineOption -EditMode vi
 
-# Credential helper functions
-function Unlock-Vault {
-    if(!$env:BW_SESSION){
-        $env:BW_SESSION = bw --raw login
-    }
-}
-
 # Set shortcuts
-
 Set-Alias -Name stop -Value Stop-Computer
+Set-Alias -Name restart -Value Restart-Computer
 
 
