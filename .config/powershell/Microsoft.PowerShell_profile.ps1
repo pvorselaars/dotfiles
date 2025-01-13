ebf9f7725e4a755abd956640f428408e1f83ee3c
Perry Vorselaars <p.vorselaars@gmail.com>
2025-01-13 17:41:29 +0000

    Fixed aliases

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -15,12 +15,45 @@ function prompt {
 	"$date $cwd $($PSStyle.Foreground.Magenta)$branch$($PSStyle.Reset)> "
 }
 
-Set-Alias -Name k -Value kubectl
-Set-Alias -Name kns -Value "kubectl config set-context --current --namespace "
-Set-Alias -Name kctx -Value "kubectl config use-context "
-Set-Alias -Name kgr -Value "kubectl get all,cm,secret,ing "
+function Set-Namespace {
+	kubectl config set-context --current --namespace $args
+}
 
-Set-Alias -Name gs -Value "git status"
-Set-Alias -Name gca -Value "git commit -am "
-Set-Alias -Name g -Value "git pull"
-Set-Alias -Name gd -Value "git diff"
+function Get-Resources {
+	kubectl get all,cm,secret,ing
+}
+
+function Get-Context {
+	kubectl config get-contexts
+}
+
+function Set-Context {
+	kubectl config use-context $args
+}
+
+function Git-Status {
+	git status
+}
+
+function Git-Commit {
+	git commit -am "$args"
+}
+
+function Git-Pull {
+	git pull
+}
+
+function Git-Diff {
+	git diff
+}
+
+Set-Alias -Name k -Value kubectl
+Set-Alias -Name kns -Value Set-Namespace
+Set-Alias -Name kctx -Value Set-Context
+Set-Alias -Name kctxs -Value Get-Context
+Set-Alias -Name kgr -Value Get-Resources
+
+Set-Alias -Name gs -Value Git-Status
+Set-Alias -Name gca -Value Git-Commit
+Set-Alias -Name g -Value Git-Pull
+Set-Alias -Name gd -Value Git-Diff
