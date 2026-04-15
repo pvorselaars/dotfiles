457d5998834d9116726997ca5b6c776389095701
Perry Vorselaars <p.vorselaars@gmail.com>
2026-04-15 19:42:12 +0000

    Update nvim

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,21 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-	{ "mason-org/mason.nvim", opts = {} },
-	"neovim/nvim-lspconfig",
+    {
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+	    { "mason-org/mason.nvim", opts = {} },
+	    "neovim/nvim-lspconfig",
+	},
+	opts = {
+	    ensure_installed = { "lua_ls" }
+	},
+	automatic_enable = true
     },
-    opts = {
-	ensure_installed = { "lua_ls" }
-    },
-    automatic_enable = true
+    {
+	"seblyng/roslyn.nvim",
+	---@module 'roslyn.config'
+	---@type RoslynNvimConfig
+	opts = {
+	    -- your configuration comes here; leave empty for default settings
+	},
+    }
 }
---
+++ config/nvim/lua/plugins/nerdy.lua
@@ -0,0 +1,17 @@
+return
+{
+    '2kabhishek/nerdy.nvim',
+    dependencies = {
+        'folke/snacks.nvim',
+    },
+    cmd = 'Nerdy',
+    opts = {
+        max_recents = 30, -- Configure recent icons limit
+        copy_to_clipboard = false, -- Copy glyph to clipboard instead of inserting
+        copy_register = '+', -- Register to use for copying (if `copy_to_clipboard` is true)
+    },
+    keys = {
+        { '<leader>in', ':Nerdy list<CR>', desc = "Browse nerd icons" },
+        { '<leader>iN', ':Nerdy recents<CR>', desc = "Browse recent nerd icons" },
+    },
+}
